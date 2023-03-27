abf13b0ab7f997925336ac854097afbcbb6c53d1
gonzacebre <76702750+gonzaCebre@users.noreply.github.com>
2023-03-26 21:32:06 -0300

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,32 @@ import { usePosts } from "../context/postContext";
 import { VscEmptyWindow } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 import { PostCard } from "../components/PostCard";
+import { useEffect } from "react";
 
 export function HomePage() {
   const { posts } = usePosts();
 
+  /*   useEffect(() => {
+    const fetchCheckout = async () => {
+      const response = await fetch("/api/checkout", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          user,
+          turno,
+        }),
+      });
+      const data = await response.json();
+
+      console.log(data.global);
+
+      if (data.global) {
+      }
+    };
+  }, []);
+ */
   if (posts.length === 0)
     return (
       <div>
